c9375e94f169d5e98465c0016be8c89fcbb93c34
lars-alfonse <lars.alfonse@gmail.com>
2017-08-08 16:05:52 -0500

    created purchase chance function, added modifer variables to weather and orangeade

--- OrangeadeStand/Customer.cs
+++ OrangeadeStand/Customer.cs
@@ -8,5 +8,87 @@ namespace OrangeadeStand
 {
     class Customer
     {
+        private int maxPrice;
+        private int thirstLevel;
+        private bool willBuy;
+        private int tartPreference;
+        private int sweetPreference;
+        private string pulpPreference;
+        private double purchaseChance;
+        Random random = new Random();
+
+        public Customer(Weather currentWeather, Orangeade currentOrangeade)
+        {
+            DetermineMaxPrice();
+            DetermineThirstLevel(currentWeather);
+            tartPreference = DetermineModifiers();
+            sweetPreference = DetermineModifiers();
+        }
+        private void DetermineMaxPrice()
+        {
+            maxPrice = (int)Math.Floor(purchaseChance / 2);
+        }
+        private void DeterminePulpPreference()
+        {
+            List<string> pulpOptions = new List<string> { "some", "none", "alot" };
+            int pulpSelector;
+            pulpSelector = random.Next(1, 3);
+            pulpPreference = pulpOptions[pulpSelector];
+
+        }
+        private void DetermineThirstLevel(Weather currentWeather)
+        {
+            double thirstModifier;
+            thirstModifier = ((currentWeather.temperature - 50) / 60) * 100 + random.Next(-10, 10);
+            thirstLevel = (int)Math.Floor(thirstModifier);
+        }
+        private int DetermineModifiers()
+        {
+            int modifier;
+            modifier = random.Next(1, 100);
+            return modifier;
+        }
+        private double CreatePurchaseChanceModifier(double orangadeTrait, int customerPreference)
+        {
+            double purchaseChanceModifier;
+            purchaseChanceModifier = (double)Math.Abs(orangadeTrait - customerPreference) / customerPreference *-1;
+            return purchaseChanceModifier;
+        }
+        private double CreatePulpPreferenceModifier(string pulpLevel, string pulpPreference)
+        {
+            if (pulpPreference == pulpLevel)
+            {
+                return .2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private double CreateWeatherModifier(Weather currentWeather)
+        {
+            if (currentWeather.isNice)
+            {
+                return .2;
+            }
+            else
+            {
+                return -.2;
+            }
+        }
+        private void CollectPurchaseChanceModifiers(Orangeade currentOrangeade, Weather currentWeather)
+        {
+            double tartModifier;
+            double sweetModifier;
+            double thirstModifier;
+            double pulpModifier;
+            double weatherModifier;
+            tartModifier = CreatePurchaseChanceModifier(currentOrangeade.tart, tartPreference);
+            sweetModifier = CreatePurchaseChanceModifier(currentOrangeade.sweet, sweetPreference);
+            thirstModifier = CreatePurchaseChanceModifier(currentOrangeade.refresh, thirstLevel);
+            pulpModifier = CreatePulpPreferenceModifier(currentOrangeade.pulp, pulpPreference);
+            weatherModifier = CreateWeatherModifier(currentWeather);
+            purchaseChance = (1 + tartModifier + sweetModifier + thirstModifier + pulpModifier + weatherModifier) * 100;
+        }
     }
 }
