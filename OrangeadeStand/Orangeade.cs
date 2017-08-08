1c37b53a84d95bf93c144726dd878f73cda5ef3b
lars-alfonse <lars.alfonse@gmail.com>
2017-08-08 11:50:08 -0500

    added traits, recipie, cost, and ways to obtain those parameters to Orangeade class

--- OrangeadeStand/Orangeade.cs
+++ OrangeadeStand/Orangeade.cs
@@ -8,5 +8,40 @@ namespace OrangeadeStand
 {
     class Orangeade
     {
+        private double cost;
+        private double oranges;
+        private double sugar;
+        private double ice;
+        private string pulp;
+        private double tart;
+        private double sweet;
+        private double refresh;
+
+        private void SetRecipie()
+        {
+            Console.WriteLine("How many oranges would you like per pitcher?");
+            oranges = double.Parse(Console.ReadLine());
+            Console.WriteLine("How much sugar would you like per pitcher? (in cups)");
+            sugar = double.Parse(Console.ReadLine());
+            Console.WriteLine("How much pulp will you leave in? (None, Some, or Alot)");
+            pulp = Console.ReadLine().ToLower();
+            Console.WriteLine("How many icecubes will you put in each cup?");
+            ice = double.Parse(Console.ReadLine());
+        }
+        private void SetCost()
+        {
+            Console.WriteLine("How many Reales will you sell your orangeade for?");
+            cost = double.Parse(Console.ReadLine());
+        }
+        private void GetTraits()
+        {
+            refresh = CalculateIngredientPercentage(ice);
+            sweet = CalculateIngredientPercentage(sugar);
+            tart = CalculateIngredientPercentage(oranges);
+        }
+        private double CalculateIngredientPercentage(double ingredient)
+        {
+            return (ingredient / (sugar + ice + oranges)) * 100;
+        }
     }
 }
